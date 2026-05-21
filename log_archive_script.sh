f9c771ebc6b286353c863a206e39a6aaf6e42485
YassinMohamed <52466287+yasso2weny@users.noreply.github.com>
2026-05-21 10:10:51 -0700

    Update log_archive_script.sh

--- log_archive_script.sh
+++ log_archive_script.sh
@@ -2,15 +2,24 @@
 
 set -euo pipefail
 
-# --- A variable for where the logs live ---
+# --- A variable for where the logs live --------------------------------------------
 LOG_DIR="/var/log"
 
-# --- A variable for where the compresse archives will be saved ---
+# --- A variable for where the compresse archives will be saved ---------------------
 ARCHIVE_DIR="/var/log_archives"
 
-# --- Build the archive filename with current date and time of compression ---
+# --- Build the archive filename with current date and time of compression -----------
 TIMESTAMP=$(date +"%Y%m%d_%H%M%S")
 ARCHIVE_NAME="logs_archive_${TIMESTAMP}.tar.gz"
 
-# --- Create the archives folder if it doesn't exist yet ---
+# --- Create the archives folder if it doesn't exist yet ----------------------------
 sudo mkdir -p "$ARCHIVE_DIR"
+
+# --- Compress /var/log into a .tar.gz file 
+sudo tar -czf "${ARCHIVE_DIR}/${ARCHIVE_NAME}" "$LOG_DIR"
+
+# --- Empty each log file (truncate instead of delete) ------------------------------
+# Deleting breaks apps that are actively writing to those files
+# Truncating empties them but keeps the file in place 
+
+# --- Record what happened and when -------------------------------------------------
